6d53057d712c951c469c40dfa4545a8db42f7e03
Robin Ghosh <ghoshr@users.noreply.github.com>
2026-01-14 14:34:38 -0600

    Add fetch request output to main function

--- FileName.cpp
+++ FileName.cpp
@@ -4,6 +4,7 @@ int main() {
 	cout << "this is a test" << endl;
 	cout << "this is an update" << endl;
 	cout << "this is a pull request" << endl;
+	cout << "this is a fetch request" << endl;
 
 	return 0;
 }
